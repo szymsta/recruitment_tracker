41d37e93cfec0b4b76e35ce6f43b51afb666ca4d
unknown <stanislawszymanski89@gmail.com>
2025-01-23 18:54:59 +0100

    Add 'add_offer' function to project.py

--- rekrutacja/project.py
+++ rekrutacja/project.py
@@ -1,16 +1,22 @@
+# Libraries
 import sys
 
 
+# Empty list
+offers = []
+
+# Main function to initialize and run the script
 def main():
     while True:
         welcome()
         choice = input("Enter your choice (1-5): ").strip()
-        menu(choice)
+        choices(choice)
 
 
+# Print menu function
 def welcome():
     print("*" * 30)
-    print("Welcom to recruitment tracker")
+    print("Recruitment tracker")
     print("*" * 30)
     print("Press number to choice what you want to do:")
     print("1. Add an offer I applied for")
@@ -20,9 +26,11 @@ def welcome():
     print("5. Exit program")
 
 
-def menu(item):
+# 
+def choices(item):
+
     if item == "1":
-        pass
+        add_offer()
     elif item == "2":
         pass
     elif item == "3":
@@ -35,6 +43,29 @@ def menu(item):
     else:
         print("Invalid choice, Please try 1-5")
 
+
+def add_offer():
+    # Details from user:
+    print("\nEnter the details for the offer you applied for:")
+
+    url = input("URL for the offer: ")
+    position = input("Position I applied for: ")
+    date = input("Application date (YYYY-MM-DD): ")
+    salary_min = input("Minimum salary: ")
+    salary_max = input("Maximum salary: ")
+
+    # Create dict for offer
+    offer = {
+        "link": url,
+        "position": position,
+        "date": date,
+        "salary_min": salary_min,
+        "salary_max": salary_max
+    }
+
+    # Add offer to offer list
+    offers.append(offer)
+    print(f"\nOffer for '{position}' has been added.")
 
 if __name__ == "__main__":
     main()
